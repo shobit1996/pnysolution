2fa94cf301ab3104227509dd5255b0b4d2f8dbdf
shobit1996 <shobhitsharma120@gmail.com>
2026-05-30 04:03:43 +0530

    update settings.py1

--- pny_backend/settings.py
+++ pny_backend/settings.py
@@ -21,7 +21,8 @@ SECRET_KEY = 'django-insecure-pny-talent-solutions-super-secret-key-yogi'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = ['pnysolution-production.up.railway.app']
+# ALLOWED_HOSTS = ['pnysolution-production.up.railway.app']
+ALLOWED_HOSTS = ['pnysolution-production.up.railway.app', 'localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "https://www.pnysolutions.com",
 ]
